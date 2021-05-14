153aadc243c00fffab88cf907e5c3766c443edbd
James Wheadon <james.wheadon@yahoo.com>
2021-05-14 12:10:40 +0100

    test for 404 error

--- test_app.py
+++ test_app.py
@@ -4,3 +4,8 @@ def test_api_get_home(api):
     res = api.get('/')
     assert res.json == {'message': 'Hello from Community Cook API!'}
     assert res.status == '200 OK'
+
+def test_api_404(api):
+    res = api.get('/incorrect_route')
+    assert 'Error occurred' in res.json['message']
+    assert res.status == '404 NOT FOUND'
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,3 @@
-from app import app
+from app import app # type: ignore
 
 app.run()
