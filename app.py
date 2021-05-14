109daff2ceea4f578c34b4c4bb42b6957d455fda
James Wheadon <james.wheadon@yahoo.com>
2021-05-14 12:13:34 +0100

    test for 404 error

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request # type: ignore
 from flask_cors import CORS # type: ignore
+from werkzeug import exceptions # type: ignore
 
 app = Flask(__name__)
 CORS(app)
@@ -7,6 +8,11 @@ CORS(app)
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({'message': 'Hello from Community Cook API!'}), 200
+
+@app.errorhandler(exceptions.NotFound)
+def handle_404(err):
+    return {'message': f'Error occurred: {err}'}, 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
