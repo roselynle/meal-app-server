f3281e1f7e31f38987e854e35f4e71f6b2510fd3
James Wheadon <james.wheadon@yahoo.com>
2021-05-17 12:34:59 +0100

    test for retrieving recipes, no query

--- test_app.py
+++ test_app.py
@@ -9,10 +9,13 @@ def test_api_new_recipe(api):
     mock_data = json.dumps({"Instructions": "", "dietary-req": [{"Vegan": True}, {"Vegetarian": True}, {"Pescatarian": False}, {"Gluten-free": False}, {"Dairy-free": False}, {"Nut-free": True}], "ingredients": [], "recipeDescription": "test", "recipeName": "test pasta"})
     mock_headers = {'Content-Type': 'application/json'}
     res = api.post('/recipes/new/', data=mock_data, headers=mock_headers)
-    print(res)
     assert res.status == '201 CREATED'
     assert 'New recipe added' in res.json['message']
-    
+
+def test_api_get_recipes(api):
+    res = api.get('/recipes/')
+    assert res.json[0]["title"] == "test pasta"
+    assert res.status == '200 OK'
 
 def test_api_404(api):
     res = api.get('/incorrect_route')
